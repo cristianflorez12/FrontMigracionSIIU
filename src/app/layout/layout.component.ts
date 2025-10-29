a7764433f26001f6ec9903a02f54b29c43471694
cristianmflorez <cristianmflorez@gmail.com>
2025-10-29 15:59:43 -0500

    steps

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,14 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { Router, RouterOutlet } from '@angular/router';
 import { MatStepperModule } from '@angular/material/stepper';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [RouterOutlet, MatStepperModule, MatIconModule, MatButtonModule],
+  imports: [
+    CommonModule,
+    RouterOutlet,
+    MatStepperModule,
+    MatIconModule,
+    MatButtonModule,
+  ],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.scss',
 })
-export class LayoutComponent {}
+export class LayoutComponent {
+  private router = inject(Router);
+
+  get showStepper(): boolean {
+    return this.router.url !== '/';
+  }
+}
